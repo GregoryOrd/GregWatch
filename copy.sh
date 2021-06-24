06848f6b6a1e9898298d6eca250a175b294acc4b
gregoryord <g.ord@hotmail.ca>
2021-06-23 19:32:30 -0600

    Update copy.sh and sim makefile

--- copy.sh
+++ copy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-scp -r /mnt/c/GregWatch pi@192.168.0.121:~/GregWatch
+ssh pi@192.168.0.121 'mkdir -p ~/GregWatch/src; mkdir -p ~/GregWatch/lib/plugins'
+scp -r /mnt/c/GregWatch/src pi@192.168.0.121:~/GregWatch/
+scp -r /mnt/c/GregWatch/config pi@192.168.0.121:~/GregWatch/
+scp /mnt/c/GregWatch/lib/plugins/RPToArduinoLoaderPlugin_RP.so pi@192.168.0.121:~/GregWatch/lib/plugins/RPToArduinoLoaderPlugin_RP.so
