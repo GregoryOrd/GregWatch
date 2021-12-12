5e937e99092f27001c43055f0b2af9c0c91eadb0
gregoryord <g.ord@hotmail.ca>
2021-12-11 17:52:34 -0700

    Add wiring note to lightSensor.h until PCB schematics can be updated

--- src/stopWatch/lightSensor/lightSensor.h
+++ src/stopWatch/lightSensor/lightSensor.h
@@ -1,6 +1,14 @@
 #ifndef LIGHTSENSOR_H
 #define LIGHTSENSOR_H
 
+/*
+Note about wiring:
+One leg of photoresistor gets 5V power.
+The other leg of the photoresitor is connected to the ADC5 pin.
+The pin that is connected to the ADC5 pin also has a 220 Ohm resistor connecting
+it to ground.
+*/
+
 #ifdef TEST
 #include "../../sim/hardwareSim.h"
 #else
